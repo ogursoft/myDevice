cebc02d7e47f4933aa07c94db5d39a3fa6adb857
Владислав Огурцов <ogursoft@bk.ru>
2021-11-04 18:52:47 +0300

    Update time_func.h

--- time_func.h
+++ time_func.h
@@ -1,1 +1,7 @@
-String uptime(unsigned long currentmillis);
+#ifndef __TIME_FUNC
+#define __TIME_FUNC
+
+extern String uptime(unsigned long currentmillis);
+extern String fDateTime(long cDay, long cMonth, long cYear, long cHour, long cMin, long cSec);
+
+#endif  
